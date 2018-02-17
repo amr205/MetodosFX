34a5df3d64dae6faa30f8a79ba81b4167300de4e
Unknown <alejandromedinareyesmx@gmail.com>
2018-02-17 14:30:32 -0600

    added fx ids

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.*;
+
+
 public class Controller {
+    @FXML
+    TextField equationField, drawStart, drawEnd, solveStart, solveEnd;
+
+    @FXML
+    ComboBox methodBox;
+
+    @FXML
+    Label resultLabel;
+
+    @FXML
+    LineChart lineChart;
+
+    @FXML
+    TableView methodTable;
+
+    @FXML
+    Button calculateBtn;
+
+    @FXML
+    protected void calculateResult(){
+        System.out.println(equationField.getText());
+    }
+
+
 }
