f6d4c2c763b8b5c915eb4d475aa3c809814ba166
Unknown <alejandromedinareyesmx@gmail.com>
2018-02-16 21:39:26 -0600

    added exp4j library

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,13 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        double result = new ExpressionBuilder("x+y").variables("x","y").build().
+                setVariable("x",15).setVariable("y",6).evaluate();
+
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello World "+result);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
